7a3a7cd268e7394ad6ae217c618cb0a6a53a97c7
Elina Kuznetsova <elina.kuznetcova@gmail.com>
2022-02-02 21:07:29 -0800

    Create Day_5:_Loops.py

---
+++ 30_Days_of_Code/Day_5:_Loops.py
@@ -0,0 +1,14 @@
+# Task
+# Given an integer, n , print its first 10 multiples. 
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+for i in range (1,11):
+    print('{} x {} = {}'.format(n, i, (n*i)))
